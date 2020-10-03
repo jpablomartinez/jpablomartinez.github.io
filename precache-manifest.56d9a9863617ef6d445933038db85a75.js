689aec2a5b4b58e50e532792b5cce499cb281153
juanpablo <jp.martinez.colombo@gmail.com>
2020-10-03 15:28:46 -0300

    Updates

---
+++ precache-manifest.56d9a9863617ef6d445933038db85a75.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0cbfcec836da849e36d3e218448bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3fb6cd7aad36de4cf98",
+    "url": "/static/css/main.a8d9e1ba.chunk.css"
+  },
+  {
+    "revision": "710694add6d402b4158e",
+    "url": "/static/js/2.ecae6880.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ecae6880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fb6cd7aad36de4cf98",
+    "url": "/static/js/main.6f3d5e1d.chunk.js"
+  },
+  {
+    "revision": "a3c2100dc2b01e1a257a",
+    "url": "/static/js/runtime-main.8a30fe64.js"
+  },
+  {
+    "revision": "f149ef2074aea621b6a093b34257a45a",
+    "url": "/static/media/Mendeleev.f149ef20.png"
+  },
+  {
+    "revision": "de85f64cf005e0b3f0702f5f77243d5e",
+    "url": "/static/media/PSHC.de85f64c.png"
+  },
+  {
+    "revision": "8fc7f883f7dec030fb03e0a7f5acafda",
+    "url": "/static/media/home.8fc7f883.jpg"
+  },
+  {
+    "revision": "476370adde2c7b29d3c4db88c6d5ed00",
+    "url": "/static/media/noImage.476370ad.png"
+  },
+  {
+    "revision": "21536d9e4da8653d15e3f7787d30eeb9",
+    "url": "/static/media/toec3.21536d9e.png"
+  },
+  {
+    "revision": "fbda94dfdc08a32a892e460b482615d4",
+    "url": "/static/media/user.fbda94df.png"
+  }
+]);
